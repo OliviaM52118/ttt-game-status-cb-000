0654886c50a6890877275840135c1fde4456326e
Olivia Macedo <liom62313@gmail.com>
2019-01-09 23:24:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,56 +27,56 @@ def won?(board)
   char1 = "X"
   char2 = "O"
   answer = []
-  if(temp1[0] == temp1[1] && temp1[0] == temp1[2])
+  if(temp1[0] == temp1[1] && temp1[0] == temp1[2]) #top row
     if(temp1[0] == char1 || temp1[0] == char2)
       answer = WIN_COMBINATIONS[0]
       return answer
     else
       return false
     end
-  elsif(temp2[0] == temp2[1] && temp2[0] == temp2[2])
+  elsif(temp2[0] == temp2[1] && temp2[0] == temp2[2]) #middle row
     if(temp2[0] == char1 || temp2[0] == char2)
       answer = WIN_COMBINATIONS[1]
       return answer
     else
       return false
     end
-  elsif(temp3[0] == temp3[1] && temp3[0] == temp3[2])
+  elsif(temp3[0] == temp3[1] && temp3[0] == temp3[2]) #bottom row
     if(temp3[0] == char1 || temp3[0] == char2)
       answer = WIN_COMBINATIONS[2]
       return answer
     else
       return false
     end
-  elsif(temp4[0] == temp4[1] && temp4[0] == temp4[2])
+  elsif(temp4[0] == temp4[1] && temp4[0] == temp4[2]) #left column
     if(temp4[0] == char1 || temp4[0] == char2)
       answer = WIN_COMBINATIONS[3]
       return answer
     else
       return false
     end
-  elsif(temp5[0] == temp5[1] && temp5[0] == temp5[2])
+  elsif(temp5[0] == temp5[1] && temp5[0] == temp5[2]) #center column
     if(temp5[0] == char1 || temp5[0] == char2)
       answer = WIN_COMBINATIONS[4]
       return answer
     else
       return false
     end
-  elsif(temp6[0] == temp6[1] && temp6[0] == temp6[2])
+  elsif(temp6[0] == temp6[1] && temp6[0] == temp6[2]) #right column
     if(temp6[0] == char1 || temp6[0] == char2)
       answer = WIN_COMBINATIONS[5]
       return answer
     else
       return false
     end
-  elsif(temp7[0] == temp7[1] && temp7[0] == temp7[2])
+  elsif(temp7[0] == temp7[1] && temp7[0] == temp7[2]) #top-left diagonal
     if(temp7[0] == char1 || temp7[0] == char2)
       answer = WIN_COMBINATIONS[6]
       return answer
     else
       return false
     end
-  elsif(temp8[0] == temp8[1] && temp8[0] == temp8[2])
+  elsif(temp8[0] == temp8[1] && temp8[0] == temp8[2]) #top-right diagonal
     if(temp8[0] == char1 || temp8[0] == char2)
       answer = WIN_COMBINATIONS[7]
       return answer
