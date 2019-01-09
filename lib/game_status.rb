989f452fda7615211a86d2d6432fa57895226a66
Olivia Macedo <liom62313@gmail.com>
2019-01-09 20:58:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -30,56 +30,56 @@ def won?(board)
   if(temp1[0] == temp1[1] && temp1[0] == temp1[2])
     if(temp1[0] == char1 || temp1[0] == char2)
       answer = WIN_COMBINATIONS[0]
-      return true
+      return answer
     else
       return false
     end
   elsif(temp2[0] == temp2[1] && temp2[0] == temp2[2])
     if(temp2[0] == char1 || temp2[0] == char2)
       answer = WIN_COMBINATIONS[1]
-      return true
+      return answer
     else
       return false
     end
   elsif(temp3[0] == temp3[1] && temp3[0] == temp3[2])
     if(temp3[0] == char1 || temp3[0] == char2)
       answer = WIN_COMBINATIONS[2]
-      return true
+      return answer
     else
       return false
     end
   elsif(temp4[0] == temp4[1] && temp4[0] == temp4[2])
     if(temp4[0] == char1 || temp4[0] == char2)
       answer = WIN_COMBINATIONS[3]
-      return true
+      return answer
     else
       return false
     end
   elsif(temp5[0] == temp5[1] && temp5[0] == temp5[2])
     if(temp5[0] == char1 || temp5[0] == char2)
       answer = WIN_COMBINATIONS[4]
-      return true
+      return answer
     else
       return false
     end
   elsif(temp6[0] == temp6[1] && temp6[0] == temp6[2])
     if(temp6[0] == char1 || temp6[0] == char2)
       answer = WIN_COMBINATIONS[5]
-      return true
+      return answer
     else
       return false
     end
   elsif(temp7[0] == temp7[1] && temp7[0] == temp7[2])
     if(temp7[0] == char1 || temp7[0] == char2)
       answer = WIN_COMBINATIONS[6]
-      return true
+      return answer
     else
       return false
     end
   elsif(temp8[0] == temp8[1] && temp8[0] == temp8[2])
     if(temp8[0] == char1 || temp8[0] == char2)
       answer = WIN_COMBINATIONS[7]
-      return true
+      return answer
     else
       return false
     end
