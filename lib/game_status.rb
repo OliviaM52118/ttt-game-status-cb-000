4ddf861e0dd8b1bc854a8a7cf85919481038ca57
Olivia Macedo <liom62313@gmail.com>
2019-01-10 00:26:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,6 +27,7 @@ def won?(board)
   char1 = "X"
   char2 = "O"
   answer = []
+  myanswer = []
   if(temp1[0] == temp1[1] && temp1[0] == temp1[2]) #top row
     if(temp1[0] == char1 || temp1[0] == char2)
       answer = WIN_COMBINATIONS[0]
@@ -43,8 +44,8 @@ def won?(board)
     end
   elsif(temp3[0] == temp3[1] && temp3[0] == temp3[2]) #bottom row
     if(temp3[0] == char1 || temp3[0] == char2)
-      answer = WIN_COMBINATIONS[2]
-      return answer
+      myanswer = WIN_COMBINATIONS[2]
+      return myanswer
     else
       return false
     end
@@ -64,8 +65,8 @@ def won?(board)
     end
   elsif(temp6[0] == temp6[1] && temp6[0] == temp6[2]) #right column
     if(temp6[0] == char1 || temp6[0] == char2)
-      answer = WIN_COMBINATIONS[5]
-      return answer
+      myanswer = WIN_COMBINATIONS[5]
+      return myanswer
     else
       return false
     end
